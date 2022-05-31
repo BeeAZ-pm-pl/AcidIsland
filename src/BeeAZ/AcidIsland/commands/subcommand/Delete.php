e576385412461cbfa18c3a9d4d64e8a70f0154ee
Nguyễn Đạt <89953941+BeeAZ-pm-pl@users.noreply.github.com>
2022-05-31 17:09:42 +0700

    Update Delete.php

--- src/BeeAZ/AcidIsland/commands/subcommand/Delete.php
+++ src/BeeAZ/AcidIsland/commands/subcommand/Delete.php
@@ -18,49 +18,49 @@ class Delete{
  public function onCommand(CommandSender $player, array $args){
   $name = strtolower($player->getName());
   $ai = AcidIsland::getInstance();
-  if($player->hasPermission("acidisland.delete")){
-  if(count($args) < 2){
-  if($ai->isIsland($name)){
-   $this->removeIsland($name);
-   $player->sendMessage($ai->cfg->get("ISLAND-DELETE"));
-  return true;
-  }
-  }else{
-  $a = strtolower(implode(" ", array_slice($args, 1)));
-  if($player->hasPermission("acidisland.delete.other")){
-  if($ai->isIsland($a)){
-   $this->removeIsland($a);
-   $player->sendMessage($ai->cfg->get("ISLAND-DELETE-OTHER"));
-  }else{
-   $player->sendMessage($ai->cfg->get("ISLAND-NOTFOUND"));
-   }
+    if($player->hasPermission("acidisland.delete")){
+      if(count($args) < 2){
+        if($ai->isIsland($name)){
+          $this->removeIsland($name);
+          $player->sendMessage($ai->cfg->get("ISLAND-DELETE"));
+       return true;
+    }
+       }else{
+         $a = strtolower(implode(" ", array_slice($args, 1)));
+           if($player->hasPermission("acidisland.delete.other")){
+             if($ai->isIsland($a)){
+               $this->removeIsland($a);
+               $player->sendMessage($ai->cfg->get("ISLAND-DELETE-OTHER"));
+      }else{
+        $player->sendMessage($ai->cfg->get("ISLAND-NOTFOUND"));
+      }
+     return true;
+    }
    return true;
-   }
-  return true;
- }
+  }
  }
 }
  
  public function removeIsland($name){
- if(Server::getInstance()->getWorldManager()->isWorldLoaded("ai-". $name)){
-  $world = Server::getInstance()->getWorldManager()->getWorldByName("ai-".$name);
-  if(count($world->getPlayers()) > 0){
-  foreach($world->getPlayers() as $players){
-    $players->teleport(Server::getInstance()->getWorldManager()->getDefaultWorld()->getSafeSpawn());
-   }
-  }
+  if(Server::getInstance()->getWorldManager()->isWorldLoaded("ai-". $name)){
+    $world = Server::getInstance()->getWorldManager()->getWorldByName("ai-".$name);
+      if(count($world->getPlayers()) > 0){
+        foreach($world->getPlayers() as $players){
+          $players->teleport(Server::getInstance()->getWorldManager()->getDefaultWorld()->getSafeSpawn());
+      }
+    }
   Server::getInstance()->getWorldManager()->unloadWorld($world);
  }
- $files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($worldPath = Server::getInstance()->getDataPath() . "/worlds/ai-$name", FilesystemIterator::SKIP_DOTS), RecursiveIteratorIterator::CHILD_FIRST);
-        foreach($files as $fileInfo) {
-            if($filePath = $fileInfo->getRealPath()) {
-                if($fileInfo->isFile()) {
-                    unlink($filePath);
-                } else {
-                    rmdir($filePath);
-                }
-            }
+  $files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($worldPath = Server::getInstance()->getDataPath() . "/worlds/ai-$name", FilesystemIterator::SKIP_DOTS), RecursiveIteratorIterator::CHILD_FIRST);
+    foreach($files as $fileInfo) {
+      if($filePath = $fileInfo->getRealPath()) {
+        if($fileInfo->isFile()) {
+          unlink($filePath);
+       }else{
+          rmdir($filePath);
+          }
         }
+     }
     rmdir($worldPath);
-}
+  }
 }
