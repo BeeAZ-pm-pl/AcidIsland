67ce29b4df12c88ac7736cd29c4b78b270e6af31
Nguyễn Đạt <89953941+BeeAZ-pm-pl@users.noreply.github.com>
2022-05-31 16:56:56 +0700

    Clean

--- src/BeeAZ/AcidIsland/commands/AICommand.php
+++ src/BeeAZ/AcidIsland/commands/AICommand.php
@@ -43,61 +43,61 @@ class AICommand extends Command implements PluginOwned{
   private $about;
   
   public function __construct(){
-  parent::__construct("acidisland", "AcidIsland Command", null, ["ai", "ac"]);
-  $this->join = new Join();
-  $this->help = new Help();
-  $this->add = new Add();
-  $this->remove = new Remove();
-  $this->teleport = new Teleport();
-  $this->setting = new Setting();
-  $this->info = new Info();
-  $this->kick = new Kick();
-  $this->delete = new Delete();
-  $this->about = new About();
-  }
+   parent::__construct("acidisland", "AcidIsland Command", null, ["ai", "ac"]);
+    $this->join = new Join();
+    $this->help = new Help();
+    $this->add = new Add();
+    $this->remove = new Remove();
+    $this->teleport = new Teleport();
+    $this->setting = new Setting();
+    $this->info = new Info();
+    $this->kick = new Kick();
+    $this->delete = new Delete();
+    $this->about = new About();
+ }
   
   public function execute(CommandSender $player, string $label, array $args){
    if($args){
-   switch($args[0]){
-    case "join":
-    case "create":
-    case "go":
-     return $this->join->onCommand($player);
-    break;
-    case "help":
-     return $this->help->onCommand($player);
-    break;
-    case "add":
-     return $this->add->onCommand($player, $args);
-    break;
-    case "remove":
-     return $this->remove->onCommand($player, $args);
-     break;
-    case "teleport":
-    case "tp":
-     return $this->teleport->onCommand($player, $args);
-    break;
-    case "setting":
-     return $this->setting->onCommand($player, $args);
-    break;
-    case "kick":
-     return $this->kick->onCommand($player, $args);
-     break;
-    case "info":
-     return $this->info->onCommand($player, $args);
-    break;
-    case "delete":
-     return $this->delete->onCommand($player, $args);
-    break;
-    case "about":
-     return $this->about->onCommand($player);
-    break;
-   }
-}else{
-  $this->help->onCommand($player);
+     switch($args[0]){
+       case "join":
+       case "create":
+       case "go":
+         return $this->join->onCommand($player);
+       break;
+       case "help":
+         return $this->help->onCommand($player);
+       break;
+       case "add":
+         return $this->add->onCommand($player, $args);
+       break;
+       case "remove":
+         return $this->remove->onCommand($player, $args);
+       break;
+       case "teleport":
+       case "tp":
+         return $this->teleport->onCommand($player, $args);
+       break;
+       case "setting":
+         return $this->setting->onCommand($player, $args);
+       break;
+       case "kick":
+         return $this->kick->onCommand($player, $args);
+       break;
+       case "info":
+         return $this->info->onCommand($player, $args);
+       break;
+       case "delete":
+         return $this->delete->onCommand($player, $args);
+       break;
+       case "about":
+         return $this->about->onCommand($player);
+       break;
+     }
+    }else{
+      $this->help->onCommand($player);
    }
 }
- public function getOwningPlugin(): AcidIsland{
-  return $this->plugin;
- }
+  public function getOwningPlugin(): AcidIsland{
+   return $this->plugin;
+  }
 }
