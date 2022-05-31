6bb73effc03263e5e6d17ca8250678a06c360043
Nguyễn Đạt <89953941+BeeAZ-pm-pl@users.noreply.github.com>
2022-05-31 17:24:47 +0700

    Update Remove.php

--- src/BeeAZ/AcidIsland/commands/subcommand/Remove.php
+++ src/BeeAZ/AcidIsland/commands/subcommand/Remove.php
@@ -16,25 +16,25 @@ class Remove{
   if(!$player->hasPermission("acidisland.remove")){
    return true;
   }
-  $name1 = strtolower($player->getName());
-  $name2 = strtolower(implode(" ", array_slice($args, 1)));
-  $ai = AcidIsland::getInstance();
-  if($ai->isIsland($name1)){
-  $ex = explode(",", $ai->getIsland($name1)->get("member"));
-  if(in_array($name2, $ex)){
-  $im = implode(",", $ex);
-  $replace = str_replace(",".$name2, "", $im);
-  if($name2 !== $name1){
-  $ai->setData($name1, "member", $replace);
-  $player->sendMessage($ai->cfg->get("ISLAND-REMOVEMEMBER"));
-  }else{
-   $player->sendMessage($ai->cfg->get("ISLAND-REMOVEERROR"));
-  }
-  }else{
-   $player->sendMessage($ai->cfg->get("ISLAND-REMOVEERROR2"));
+    $name = strtolower($player->getName());
+    $data = strtolower(implode(" ", array_slice($args, 1)));
+    $ai = AcidIsland::getInstance();
+      if($ai->isIsland($name)){
+        $ex = explode(",", $ai->getIsland($name)->get("member"));
+          if(in_array($data, $ex)){
+            $im = implode(",", $ex);
+            $replace = str_replace(",".$data, "", $im);
+              if($data !== $name){
+                $ai->setData($name, "member", $replace);
+                $player->sendMessage($ai->cfg->get("ISLAND-REMOVEMEMBER"));
+             }else{
+               $player->sendMessage($ai->cfg->get("ISLAND-REMOVEERROR"));
+            }
+        }else{
+          $player->sendMessage($ai->cfg->get("ISLAND-REMOVEERROR2"));
+       }
+    }else{
+      $player->sendMessage($ai->cfg->get("ISLAND-NOTFOUND"));
    }
-  }else{
-  $player->sendMessage($ai->cfg->get("ISLAND-NOTFOUND"));
-  }
-}
+ }
 }
