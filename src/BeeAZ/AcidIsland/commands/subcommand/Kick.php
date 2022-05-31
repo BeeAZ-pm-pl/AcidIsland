d71711caf59f135f488e407ce60d42a46c53ce76
Nguyễn Đạt <89953941+BeeAZ-pm-pl@users.noreply.github.com>
2022-05-31 17:16:37 +0700

    Update Kick.php

--- src/BeeAZ/AcidIsland/commands/subcommand/Kick.php
+++ src/BeeAZ/AcidIsland/commands/subcommand/Kick.php
@@ -11,25 +11,25 @@ class Kick{
  
  public function onCommand(CommandSender $player, array $args){
   if(!$player->hasPermission("acidisland.kick")){
-  return true;
+    return true;
   }
   if(count($args) < 2){
-  return true;
+    return true;
   }
-  $server = Server::getInstance();
-  $ai = AcidIsland::getInstance();
-  $name = implode(" ", array_slice($args, 1));
-  if($server->getPlayerExact($name) !== null){
-  $p = $server->getPlayerExact($name);
-  if($p->getWorld()->getDisplayName() == "ai-".strtolower($player->getName())){
-  $p->teleport($server->getWorldManager()->getDefaultWorld()->getSafeSpawn());
-  $player->sendMessage($ai->cfg->get("ISLAND-KICK"));
-  $p->sendMessage($ai->cfg->get("ISLAND-KICK2"));
-  }else{
-   $player->sendMessage($ai->cfg->get("ISLAND-KICKERROR"));
-  }
+    $server = Server::getInstance();
+    $ai = AcidIsland::getInstance();
+    $name = implode(" ", array_slice($args, 1));
+      if($server->getPlayerExact($name) !== null){
+        $p = $server->getPlayerExact($name);
+         if($p->getWorld()->getDisplayName() == "ai-".strtolower($player->getName())){
+           $p->teleport($server->getWorldManager()->getDefaultWorld()->getSafeSpawn());
+           $player->sendMessage($ai->cfg->get("ISLAND-KICK"));
+           $p->sendMessage($ai->cfg->get("ISLAND-KICK2"));
+     }else{
+       $player->sendMessage($ai->cfg->get("ISLAND-KICKERROR"));
+   }
   }else{
    $player->sendMessage($ai->cfg->get("PLAYER-NOTFOUND"));
   }
-}
+ }
 }
